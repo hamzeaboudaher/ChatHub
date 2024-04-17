ec8051eb404927e35bd8cbf8d7831c757a8f1b3b
Hamze Abou Daher <hamzeaboudaher@gmail.com>
2024-04-17 07:12:48 +0200

    sendMessage function korrigiert

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -6,15 +6,12 @@ function Chat({ socket, username, room }) {
   const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
-    if (currentMessage !== "") {
+    if (currentMessage.trim() !== "") {
       const messageData = {
         room: room,
         author: username,
         message: currentMessage,
-        time:
-          new Date(Date.now()).getHours() +
-          ":" +
-          new Date(Date.now()).getMinutes(),
+        time: `${new Date().getHours()}:${new Date().getMinutes()}`,
       };
 
       await socket.emit("send_message", messageData);
@@ -27,6 +24,11 @@ function Chat({ socket, username, room }) {
     socket.on("receive_message", (data) => {
       setMessageList((list) => [...list, data]);
     });
+
+    // Cleanup function to remove event listener when component unmounts
+    return () => {
+      socket.off("receive_message");
+    };
   }, [socket]);
 
   return (
@@ -36,24 +38,23 @@ function Chat({ socket, username, room }) {
       </div>
       <div className="chat-body">
         <ScrollToBottom className="message-container">
-          {messageList.map((messageContent) => {
-            return (
-              <div
-                className="message"
-                id={username === messageContent.author ? "you" : "other"}
-              >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
-                  </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
+          {messageList.map((messageContent, index) => (
+            <div
+              key={index}
+              className="message"
+              id={username === messageContent.author ? "you" : "other"}
+            >
+              <div>
+                <div className="message-content">
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
                 </div>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </ScrollToBottom>
       </div>
       <div className="chat-footer">
@@ -61,12 +62,7 @@ function Chat({ socket, username, room }) {
           type="text"
           value={currentMessage}
           placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          // onKeyPress={(event) => {
-          //   event.key === "Enter" && sendMessage();
-          // }}
+          onChange={(event) => setCurrentMessage(event.target.value)}
         />
         <button onClick={sendMessage}>&#9658;</button>
       </div>
